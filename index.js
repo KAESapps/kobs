475ace135748be502cd8d58394feb73f7194ba7f
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2021-12-13 10:42:05 +0100

    improve repeatWhen

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.info("Loading kobs")
 const log = () => {}
 const last = require("lodash/last")
 const difference = function difference(setA, setB) {
-  setB.forEach(elm => setA.delete(elm))
+  setB.forEach((elm) => setA.delete(elm))
   return setA
 }
 
@@ -19,34 +19,36 @@ exports.observable = (arg, name) => {
     return () => obs.get()
   } else {
     const obs = new exports.Obs(arg, null, null, name)
-    return function(arg) {
+    return function (arg) {
       return arguments.length ? obs.set(arg) : obs.get()
     }
   }
 }
 
-exports.Obs = function(initValue, onUnobserved, onObserved, name) {
+exports.Obs = function (initValue, onUnobserved, onObserved, name) {
   this.name = name
   this.value = initValue
   this.observers = new Set()
   this.onUnobserved = onUnobserved
 }
 Object.assign(exports.Obs.prototype, {
-  set: function(newValue) {
+  set: function (newValue) {
     //TODO: mettre en commun avec le code de "transaction"
     transactionLevel++
     this.value = newValue
     log("source changed", this.name)
-    this.observers.forEach(observer => observer.markStale())
+    this.observers.forEach((observer) => observer.markStale())
     transactionLevel--
     if (transactionLevel === 0) {
-      autorunsNeedingRun.forEach(autorun => autorun.compute(autorun.unobserve))
+      autorunsNeedingRun.forEach((autorun) =>
+        autorun.compute(autorun.unobserve)
+      )
       autorunsNeedingRun = new Set()
-      maybeUnobserved.forEach(obs => obs.notifyIfUnobserved())
+      maybeUnobserved.forEach((obs) => obs.notifyIfUnobserved())
       maybeUnobserved = new Set()
     }
   },
-  get: function() {
+  get: function () {
     const currentlyRunning = last(currentlyRunningStack)
     if (currentlyRunning && !currentlyRunning.deps.has(this)) {
       currentlyRunning.deps.add(this)
@@ -55,13 +57,13 @@ Object.assign(exports.Obs.prototype, {
     }
     return this.value
   },
-  removeObserver: function(observer) {
+  removeObserver: function (observer) {
     log("removing deps from source", this.name, "to", observer.name)
     this.observers.delete(observer)
     log("observers of", this.name, this.observers.size, this.observers)
     if (this.observers.size === 0) maybeUnobserved.add(this)
   },
-  notifyIfUnobserved: function() {
+  notifyIfUnobserved: function () {
     // after running all autorun, if this.observers is still empty, we can notify that we are unobserved
     if (this.onUnobserved && this.observers.size === 0) {
       log("notify unobserved", this.name)
@@ -70,7 +72,7 @@ Object.assign(exports.Obs.prototype, {
   },
 })
 
-exports.Computed = function(fn, onUnobserved, onObserved, name) {
+exports.Computed = function (fn, onUnobserved, onObserved, name) {
   this.name = name
   this.stale = true
   this.observers = new Set()
@@ -78,13 +80,13 @@ exports.Computed = function(fn, onUnobserved, onObserved, name) {
   this.fn = fn
 }
 Object.assign(exports.Computed.prototype, {
-  markStale: function() {
+  markStale: function () {
     if (this.stale) return // pas besoin de recommencer si on a déjà été marqué stale
     this.stale = true
     log(this.name, "computed marked stale")
-    this.observers.forEach(observer => observer.markStale())
+    this.observers.forEach((observer) => observer.markStale())
   },
-  compute: function() {
+  compute: function () {
     log("computing", this.name)
     this.oldDeps = this.deps
     this.deps = new Set()
@@ -92,17 +94,19 @@ Object.assign(exports.Computed.prototype, {
     this.value = this.fn()
     currentlyRunningStack.pop()
     this.stale = false
-    difference(this.oldDeps, this.deps).forEach(dep => dep.removeObserver(this))
+    difference(this.oldDeps, this.deps).forEach((dep) =>
+      dep.removeObserver(this)
+    )
   },
-  removeObserver: function(observer) {
+  removeObserver: function (observer) {
     log("removing deps from computed", this.name, "to", observer.name)
     this.observers.delete(observer)
     if (this.observers.size === 0) {
-      this.deps.forEach(dep => dep.removeObserver(this))
+      this.deps.forEach((dep) => dep.removeObserver(this))
       this.stale = true // déclenche le recomputing lors de la prochaine observation
     }
   },
-  get: function() {
+  get: function () {
     const currentlyRunning = last(currentlyRunningStack)
     if (currentlyRunning && !currentlyRunning.deps.has(this)) {
       currentlyRunning.deps.add(this)
@@ -129,15 +133,15 @@ const autorun = (exports.autorun = (fn, name, after) => {
       me.canceledDuringRun = true
       log("canceling during run", name)
     } else {
-      me.deps.forEach(dep => dep.removeObserver(me))
+      me.deps.forEach((dep) => dep.removeObserver(me))
       autorunsNeedingRun.delete(me)
       me.deps = new Set()
-      maybeUnobserved.forEach(obs => obs.notifyIfUnobserved())
+      maybeUnobserved.forEach((obs) => obs.notifyIfUnobserved())
       maybeUnobserved = new Set()
       log("cancelled", name)
     }
   }
-  me.compute = cancel => {
+  me.compute = (cancel) => {
     log("computing autorun", name)
     me.oldDeps = me.deps
     me.deps = new Set()
@@ -146,27 +150,27 @@ const autorun = (exports.autorun = (fn, name, after) => {
     currentlyRunningStack.pop()
     if (me.canceledDuringRun) {
       // si on a été annulé en cours de run, il faut vider la liste des dépendances qui se sont auto enregistrées
-      me.deps.forEach(dep => dep.removeObserver(me))
+      me.deps.forEach((dep) => dep.removeObserver(me))
       autorunsNeedingRun.delete(me)
       me.deps = new Set()
       me.canceledDuringRun = false
       log("autorun cancelled during run", name)
     }
-    difference(me.oldDeps, me.deps).forEach(dep => dep.removeObserver(me))
+    difference(me.oldDeps, me.deps).forEach((dep) => dep.removeObserver(me))
     if (after) after() // éxécuté en synchrone mais sans tracking des dépendances
   }
   me.compute(me.unobserve)
   return me.unobserve
 })
 
-exports.transaction = fn => {
+exports.transaction = (fn) => {
   transactionLevel++
   fn()
   transactionLevel--
   if (transactionLevel === 0) {
-    autorunsNeedingRun.forEach(autorun => autorun.compute(autorun.unobserve))
+    autorunsNeedingRun.forEach((autorun) => autorun.compute(autorun.unobserve))
     autorunsNeedingRun = new Set()
-    maybeUnobserved.forEach(obs => obs.notifyIfUnobserved())
+    maybeUnobserved.forEach((obs) => obs.notifyIfUnobserved())
     maybeUnobserved = new Set()
   }
 }
@@ -175,11 +179,13 @@ exports.transaction = fn => {
 // si la fonction retourne un promise, la prochaine itération attend qu'il soit terminé (résolu ou rejeté)
 exports.repeatWhen = (canRun, cb, name) => {
   let repeat = true
-  const autorunFn = cancel => {
-    if (!canRun()) return
-    console.log("repeatWhen condition truthy", name)
+  const autorunFn = (cancel) => {
+    const canRunValue = canRun()
+    if (!canRunValue) return
+    console.log("repeatWhen condition truthy", { name, canRunValue })
     cancel() // on se désabonne avant d'exécuter le callback pour être sûr de ne déclencher qu'un run à la fois
-    Promise.resolve(cb()).then(() => {
+    // peut-être pas très logique de renvoyer la valeur du canRun dans le cb mais c'est pratique quand on veut exécuter le cb avec le résultat (truthy) du canRun
+    Promise.resolve(cb(canRunValue)).then(() => {
       if (!repeat) return console.log("repeatWhen canceled", name)
       autorun(autorunFn, "repeatWhenAgain") // puis on se réabonne après le run (en asynchrone), pour déclencher le suivant
     })
@@ -188,7 +194,7 @@ exports.repeatWhen = (canRun, cb, name) => {
   return () => (repeat = false)
 }
 
-exports.observeSync = function(obs, cb) {
+exports.observeSync = function (obs, cb) {
   let oldVal, newVal
   return autorun(
     () => {
@@ -202,9 +208,9 @@ exports.observeSync = function(obs, cb) {
   )
 }
 // asynchronous version of observe
-exports.observe = function(obs, cb) {
+exports.observe = function (obs, cb) {
   return () =>
-    autorun(function() {
+    autorun(function () {
       const v = obs()
       setTimeout(() => cb(v))
     })
@@ -212,15 +218,15 @@ exports.observe = function(obs, cb) {
 
 // persiste la valeur dans le local storage
 // initValue n'est utilisée que si l'observbale n'a jamais été persisté
-exports.persistedObservable = function(name, initValue) {
+exports.persistedObservable = function (name, initValue) {
   const json = window.localStorage.getItem(name)
   const value = json ? JSON.parse(json) : initValue
   const obs = new exports.Obs(value, null, null, name)
-  const setAndPersist = newValue => {
+  const setAndPersist = (newValue) => {
     window.localStorage.setItem(name, JSON.stringify(newValue))
     obs.set(newValue)
   }
-  return function(arg) {
+  return function (arg) {
     return arguments.length ? setAndPersist(arg) : obs.get()
   }
 }
